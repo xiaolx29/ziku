de10010ca84a2ec9b203a96f56f4b44acf5de0ad
xiaolingxiu <xiao.lingxiu29@gmail.com>
2024-06-14 10:25:49 +0800

    update gui_pyside

--- gui_pyside.py
+++ gui_pyside.py
@@ -1,55 +1,66 @@
+import binascii
 import sys
 from PySide6 import QtCore, QtWidgets, QtGui
 from PIL import ImageQt
 import utils
 
 class ClickableLabel(QtWidgets.QLabel):
-    def __init__(self, parent = None):
-        super().__init__(parent)
+    clicked = QtCore.Signal(QtCore.QPoint)
+    double_clicked = QtCore.Signal(QtCore.QPoint)
 
     def mousePressEvent(self, event: QtGui.QMouseEvent) -> None:
-        # 检查事件类型是否为鼠标左键点击
+        # if left mouse button clicked
         if event.button() == QtCore.Qt.LeftButton:
-            local_pos = event.position()
-            # 输出坐标
-            print(f"Mouse clicked at: ({local_pos.x()}, {local_pos.y()})")
-        # 调用基类的mousePressEvent以保持其他行为
-        super().mousePressEvent(event)
+            # emit a signal and pass mouse position
+            self.clicked.emit(event.position().toPoint())
+    
+    def mouseDoubleClickEvent(self, event: QtGui.QMouseEvent) -> None:
+        # if left mouse button double clicked
+        if event.button() == QtCore.Qt.LeftButton:
+            pass
 
 class MainWindow(QtWidgets.QMainWindow):
     filename = None
 
     def __init__(self):
         super().__init__()
-        # 设置窗口标题和大小
         self.setWindowTitle('字库工具')
-        self.setGeometry(100, 100, 400, 300)
+        self.setGeometry(100, 100, 800, 600)
 
-        # 创建一个中央部件
-        central_widget = QtWidgets.QWidget()
-        self.setCentralWidget(central_widget)
+        self.dock_widget = QtWidgets.QDockWidget('Dockable Window')
+        self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, self.dock_widget)
 
-        # 创建一个垂直布局
+        widget = QtWidgets.QWidget()
+        # self.setCentralWidget(central_widget)
+        self.dock_widget.setWidget(widget)
+
         layout = QtWidgets.QGridLayout()
-        central_widget.setLayout(layout)
+        widget.setLayout(layout)
 
-        # 创建一个按钮
         self.file_import_button = QtWidgets.QPushButton('导入字库文件')
         layout.addWidget(self.file_import_button, 0, 0)
-        self.file_import_button.clicked.connect(self.import_file)
+        self.file_import_button.clicked.connect(self.file_import_button_onclick)
 
-        # 创建一个标签
         self.filename_label = QtWidgets.QLabel('尚未导入字库文件')
         layout.addWidget(self.filename_label, 0, 1)
 
         self.page_control_spinbox = QtWidgets.QSpinBox()
+        self.page_control_spinbox.setMinimum(1)
+        self.page_control_spinbox.setEnabled(False)
         layout.addWidget(self.page_control_spinbox, 0, 2)
+        self.page_control_spinbox.valueChanged.connect(self.change_page)
 
         self.font_browse_label = ClickableLabel()
+        self.font_browse_label.setAlignment(QtCore.Qt.AlignTop)
         layout.addWidget(self.font_browse_label, 1, 0, 1, 3)
+        self.font_browse_label.clicked.connect(self.font_browse_label_onclick)
+
+        self.char_display_label = QtWidgets.QLabel()
+        self.font_browse_label.setAlignment(QtCore.Qt.AlignTop)
+        layout.addWidget(self.char_display_label, 2, 0)
 
     @QtCore.Slot()
-    def import_file(self) -> None:
+    def file_import_button_onclick(self) -> None:
         # get name of the font file
         filename, _ = QtWidgets.QFileDialog.getOpenFileName()
         if filename:  # if user has chosen a font file (not cancelled)
@@ -60,10 +71,24 @@ class MainWindow(QtWidgets.QMainWindow):
             qimage = ImageQt.ImageQt(utils.draw_page(filename = self.filename, page_index = 0))
             pixmap = QtGui.QPixmap.fromImage(qimage)
             self.font_browse_label.setPixmap(pixmap)
+            # enable page control spinbox
+            self.page_control_spinbox.setEnabled(True)
             
     @QtCore.Slot(QtCore.QPoint)
-    def on_label_clicked(self, point: QtCore.QPoint):
-        print(f"Clicked at: {point.x()}, {point.y()}")
+    def font_browse_label_onclick(self, pos: QtCore.QPoint) -> None:
+        page_index = self.page_control_spinbox.value()
+        char_no = (page_index - 1) * 1000 + (pos.y() // 16 * 50+ pos.x() // 16)
+        high_byte, low_byte = 0xA1 + char_no // 94, 0xA1 + char_no % 94
+        gb2312_bytes = bytes([high_byte, low_byte])
+        char = gb2312_bytes.decode(encoding = 'gb2312', errors = 'replace')
+        number = binascii.hexlify(gb2312_bytes).decode('ascii').upper()
+        self.char_display_label.setText(f'{char} {number}')
+
+    @QtCore.Slot(int)
+    def change_page(self, page_index: int) -> None:
+        qimage = ImageQt.ImageQt(utils.draw_page(filename = self.filename, page_index = page_index - 1))
+        pixmap = QtGui.QPixmap.fromImage(qimage)
+        self.font_browse_label.setPixmap(pixmap)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
